afa0e7526a09a44e0884447494752f5699e54cad
dignall <dennis@happyvalley.io>
2022-07-11 19:00:20 +0100

    Delete unnecessary controllers etc

--- src/main/kotlin/org/koil/image/ProfileImageRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.koil.image
-
-import org.springframework.data.repository.CrudRepository
-import java.util.*
-
-interface ProfileImageRepository : CrudRepository<ProfileImage, UUID> {
-    fun findProfileImageByAccountId(accountId: Long): ProfileImage?
-}
--- src/main/kotlin/org/koil/user/Errors.kt
+++
@@ -1,4 +0,0 @@
-package org.koil.user
-
-data class NoAccountFoundUnexpectedlyException(val accountId: Long) :
-    RuntimeException("Could not find account with ID $accountId")
