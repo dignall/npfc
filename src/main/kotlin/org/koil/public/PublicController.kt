59e96f966f71e5a76ecee4be745b98b7bf9813be
dignall <dennis@happyvalley.io>
2022-07-11 20:46:44 +0100

    Display card names on page with checkboxes

--- src/main/kotlin/org/koil/public/PublicController.kt
+++ src/main/kotlin/org/koil/public/PublicController.kt
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.servlet.ModelAndView
 
+data class IndexViewModel(val allCards: List<Card>)
+
 @Controller
 class PublicController {
     @GetMapping("/")
     fun index(): ModelAndView {
-
-        return ModelAndView("pages/index")
+        val model = IndexViewModel(Card.values().toList())
+        return ModelAndView("pages/index", mapOf("model" to model))
     }
 }
